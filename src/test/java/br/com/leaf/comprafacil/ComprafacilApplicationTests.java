a0583d001cd1d92420aa5de0b0d78d7a208e37ad
Rafael Teixeira <rafael.teixeira@libertyti.com.br>
2024-05-28 23:27:13 -0300

    :tada: Iniciando o projeto

---
+++ src/test/java/br/com/leaf/comprafacil/ComprafacilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leaf.comprafacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprafacilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
