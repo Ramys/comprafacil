329c917d90a6083439c96646f370bd95d5d6516a
Rafael Teixeira <rafael.teixeira@libertyti.com.br>
2024-06-16 19:07:12 -0300

    :construction: implementando chamada no gateway do swagger

--- api-gateway/src/main/java/br/com/leaf/apigateway/security/SecurityConfig.java
+++ api-gateway/src/main/java/br/com/leaf/apigateway/security/SecurityConfig.java
@@ -1,12 +1,10 @@
 package br.com.leaf.apigateway.security;
 
-import br.com.leaf.apigateway.filters.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -14,13 +12,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.csrf()
+                .disable()
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/usuarios/**").authenticated()
                         .requestMatchers("/usuarios/swagger-ui.html", "/usuarios/v3/api-docs").permitAll()
-                        .anyRequest().permitAll()
-                )
-                .addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+                        .anyRequest().authenticated());
         return http.build();
     }
 }
