a672579c86ff37e90b09641b038a9062a34c8808
Rafael Teixeira <rafael.teixeira@libertyti.com.br>
2024-07-16 22:33:14 -0300

    :construction: Incluíndo segurança no serviço de gestão.

---
+++ api-gateway/src/main/java/br/com/leaf/apigateway/filter/RouteValidator.java
@@ -0,0 +1,37 @@
+package br.com.leaf.apigateway.filter;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class RouteValidator {
+
+    public String getRequiredRole(ServerHttpRequest request) {
+        String path = request.getPath().toString();
+
+        if (request.getMethod() == HttpMethod.POST && path.startsWith("/products")) {
+            return "ADMIN";
+        } else if (request.getMethod() == HttpMethod.PUT && path.matches("/products/\\d+")) {
+            return "ADMIN";
+        } else if (request.getMethod() == HttpMethod.DELETE && path.matches("/products/\\d+")) {
+            return "ADMIN";
+        }
+
+        return null;
+    }
+
+    public static final List<String> openApiEndpoints = List.of(
+            "/auth/register",
+            "/auth/login",
+            "/eureka"
+    );
+
+    public Predicate<ServerHttpRequest> isSecured = request -> openApiEndpoints.stream()
+            .noneMatch(uri -> request.getURI().getPath().contains(uri));
+
+
+}
---
+++ api-gateway/src/main/java/br/com/leaf/apigateway/util/JwtUtil.java
@@ -0,0 +1,47 @@
+package br.com.leaf.apigateway.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    public String SECRET;
+
+    public void validateToken(final String token) {
+        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractRole(String token) {
+        return (String) extractAllClaims(token).get("role");
+    }
+
+    public String extractCustId(String token) {
+        return (String) extractAllClaims(token).get("cartId");
+    }
+
+    public Integer extractUserId(String token) {
+        return (Integer) extractAllClaims(token).get("userId");
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+}
